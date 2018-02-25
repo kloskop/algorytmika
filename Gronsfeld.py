3c6f68d75cc6ed8b4eb9f47298f7975bbb979c06
kloskop <35888755+kloskop@users.noreply.github.com>
2018-02-25 20:47:48 +0100

    Połącznie funkcji coding/encoding
    Zamiast osobnych funkcji coding i encoding jest teraz jedna, a wybór szyfrowania/deszyfrowania opiera się na prostym switchu
    Dodana jest funkcja demonstrująca szyfrowanie i deszyfrowanie

--- Gronsfeld.py
+++ Gronsfeld.py
@@ -1,7 +1,15 @@
-def coding(text, key):
+import sys
+
+def coding_encoding(text, key, switch):
     text_tab=[]
     key_tab=[]
     output_text_tab=[]
+    if switch == "c":
+       modyfier = 1
+    elif switch == "d":
+       modyfier = -1
+    else:
+       sys.exit("Wprowadzono błędną wartość wybory kodowania/dekodowania")
     for i in text.upper():
         text_tab+=[int(ord(i))]
     for i in str(key):
@@ -9,43 +17,22 @@ def coding(text, key):
     n = 1
     for i in range(len(text_tab)):
         if i<len(key_tab):
-            text_tab[i]+=int(key_tab[i])
+            text_tab[i]+=int(key_tab[i])*modyfier
         else:
             if i % len(key_tab) == 0 and i != len(key_tab):
                 n += 1
             j=i-n*len(key_tab)
-            text_tab[i]+=int(key_tab[j])
+            text_tab[i]+=int(key_tab[j])*modyfier
     for i in text_tab:
         output_text_tab+=chr(i)
     output_text="".join(output_text_tab)
     return output_text
 
-def encoding(text, key):
-    text_tab = []
-    key_tab = []
-    output_text_tab = []
-    for i in text.upper():
-        text_tab += [int(ord(i))]
-    for i in str(key):
-        key_tab += [i]
-    n = 1
-    for i in range(len(text_tab)):
-        if i < len(key_tab):
-            text_tab[i] -= int(key_tab[i])
-        else:
-            if i % len(key_tab) == 0 and i != len(key_tab):
-                n += 1
-            j = i - n * len(key_tab)
-            text_tab[i] -= int(key_tab[j])
-    for i in text_tab:
-        output_text_tab += chr(i)
-    output_text = "".join(output_text_tab)
-    return output_text
-
-def gronsfeld_demonstration(text): #Funkcja pokazująca szyfrowanie i deszyfrowanie
+def gronsfeld_demonstration(): #Funkcja demonstrująca szyfrowanie i deszyfrowanie
     key=input("Wprowadź klucz: ")
-    print("\nTekst po zaszyfrowaniu: \n{0}".format(coding(text,key)))
-    print("\nTekst po odczycie: \n{0} ".format(encoding(coding(text,key),key)))
+    text=import_text()
+    print("\nTekst po zaszyfrowaniu: \n{0}".format(coding_encoding(text,key,"c")))
+    print("\nTekst po odczycie: \n{0} ".format(coding_encoding(coding_encoding(text,key,"c"),key,"d")))
 
 def import_text():
     file=open(input("Podaj nazwę pliku: "))
@@ -55,5 +42,15 @@ def import_text():
         file.close()
     return text
 
+def gronsfeld(): #Funkcja umożliwiająca szyfrowanie/deszyfrowanie
+    switch = input('Dla kodowania wpisz "c", dla dekodowania "d": ')
+    text = import_text()
+    key=input("Wprowadź klucz: ")
+    if switch == "c":
+        print("Tekst po zakodowaniu:\n")
+    else:
+        print("Tekst po dekodowaniu:\n")
+    print(coding_encoding(text,key,switch))
 
-gronsfeld_demonstration(import_text())
+gronsfeld_demonstration()
+#gronsfeld()
